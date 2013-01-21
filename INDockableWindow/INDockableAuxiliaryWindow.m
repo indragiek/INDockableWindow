43de1772bd1cedce4c9c12ab1e01b566e0d45330
Indragie Karunaratne <i@indragie.com>
2013-01-21 11:36:13 -0700

    Insert view controller's view below other subviews

--- INDockableWindow/INDockableAuxiliaryWindow.m
+++ INDockableWindow/INDockableAuxiliaryWindow.m
@@ -72,13 +72,13 @@ NSString* const INDockableAuxiliaryWindowFinishedMovingNotification = @"INDockab
 		_contentImageView = [[NSImageView alloc] initWithFrame:[self.contentView bounds]];
 		_contentImageView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 		_contentImageView.image = contentView.in_image;
-		[self.contentView addSubview:_contentImageView];
+		[self.contentView addSubview:_contentImageView positioned:NSWindowBelow relativeTo:nil];
 	}
 	if (titleBarView) {
 		_titleBarImageView = [[NSImageView alloc] initWithFrame:[self.titleBarView bounds]];
 		_titleBarImageView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 		_titleBarImageView.image = titleBarView.in_image;
-		[self.titleBarView addSubview:_titleBarImageView];
+		[self.titleBarView addSubview:_titleBarImageView positioned:NSWindowBelow relativeTo:nil];
 	}
 	if (_viewController.view.superview == self.contentView) {
 		[_viewController.view removeFromSuperview];
@@ -92,14 +92,14 @@ NSString* const INDockableAuxiliaryWindowFinishedMovingNotification = @"INDockab
 	if (view) {
 		view.frame = [self.contentView bounds];
 		view.autoresizingMask = NSViewHeightSizable | NSViewWidthSizable;
-		[self.contentView addSubview:view];
+		[self.contentView addSubview:view positioned:NSWindowBelow relativeTo:nil];
 	}
 	
 	NSView *titleBarView = _viewController.titleBarView;
 	if (titleBarView) {
 		titleBarView.frame = [self.titleBarView bounds];
 		titleBarView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-		[self.titleBarView addSubview:titleBarView];
+		[self.titleBarView addSubview:titleBarView positioned:NSWindowBelow relativeTo:nil];
 	}
 	
 	[_contentImageView removeFromSuperview];
